42597750958085d2f2c2601944e952ee89dfbc73
verma-deepti <deepti.verma@droom.in>
2025-05-26 12:00:05 +0530

    OP-001 | testingg

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div> 
-      <h1 className="text-red text-bold text-[32px]">ddeeptii jiii</h1>
+      <h1 className="text-red text-bold text-[32px]">ddeeptii jiikkkki</h1>
     </div>
   );
 }
